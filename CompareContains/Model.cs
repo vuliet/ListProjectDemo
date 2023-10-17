d90558cc693072696c0e5671f6c2a1724842ac09
ThaiVu <thai35.tv@gmail.com>
2023-10-17 17:55:06 +0700

    update

---
+++ CompareContains/Model.cs
@@ -0,0 +1,25 @@
+﻿namespace CompareContains
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            Student other = (Student)obj;
+            return Id == other.Id && Name == other.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name);
+        }
+    }
+}
---
+++ CompareContains/Program.cs
@@ -0,0 +1,18 @@
+﻿using CompareContains;
+
+var students = new List<Student>
+{
+    new Student
+    {
+        Id = 1,
+        Name = "Thai"
+    },
+    new Student { Id = 2, Name = "Heo"}
+};
+
+var student = new Student { Id = 2, Name = "Heo" };
+
+var isContains = students.Contains(student);
+
+Console.WriteLine($"List student contains student : {isContains}");
+Console.ReadLine();
