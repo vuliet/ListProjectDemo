c50d87cb53814fd71d3d71e24682e3684569b9ac
ThaiVu <thai35.tv@gmail.com>
2023-10-17 15:23:29 +0700

    ...

---
+++ RandomGenerate/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Security.Cryptography;
+
+var alphaNumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_!@#$%^&*()=-~`|/><.,[]";
+
+var genString = RandomNumberGenerator.GetInt32(100);
+
+Console.WriteLine(genString);
+Console.ReadLine();
---
+++ Thread/Program.cs
@@ -0,0 +1,25 @@
+﻿Thread thread = new Thread(new ThreadStart(DoAsyncWork));
+thread.Start();
+
+// Thực hiện công việc khác trên luồng chính
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Main thread: {i}");
+    Thread.Sleep(500); // Giả lập công việc mất thời gian
+}
+
+// Chờ thread bất đồng bộ hoàn thành
+thread.Join();
+
+Console.WriteLine("Main thread finished.");
+
+static void DoAsyncWork()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"Async thread: {i}");
+        Thread.Sleep(1000); // Giả lập công việc mất thời gian
+    }
+}
+
+Console.ReadLine();
