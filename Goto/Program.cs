79754ad011c1d3e59a96ff4b60ddccfd1ad4479a
ThaiVu <thai35.tv@gmail.com>
2023-10-18 13:57:47 +0700

    create goto example

---
+++ Goto/Program.cs
@@ -0,0 +1,18 @@
+﻿var retryCount = 0;
+
+Start:
+
+try
+{
+
+}
+catch
+{
+    if (retryCount <= 4)
+    {
+        retryCount++;
+        await Task.Delay(1000);
+        goto Start;
+    }
+    throw;
+}
