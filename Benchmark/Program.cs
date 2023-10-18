765ea8ba89e9074763faab44ada0fb27e556ec47
ThaiVu <thai35.tv@gmail.com>
2023-10-18 14:24:03 +0700

    ...

---
+++ Benchmark/Program.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<Benchmarks>();
+
+public class Benchmarks
+{
+    private const int Iterations = 100;
+
+    [Benchmark]
+    public int For_Loop()
+    {
+        var count = 0;
+
+        for (int i = 0; i <= Iterations; i++)
+        {
+            count += i;
+        }
+
+        return count;
+    }
+
+    [Benchmark]
+    public int Goto_Loop()
+    {
+        var count = 0;
+        var i = 0;
+
+    Start:
+
+        if (i <= Iterations)
+        {
+            count += i;
+            i++;
+            goto Start;
+        }
+
+        return count;
+    }
+}
