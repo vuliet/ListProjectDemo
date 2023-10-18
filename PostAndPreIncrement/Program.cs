68debe0624588eb2d4929e1c3c9d387b13bbb44e
ThaiVu <thai35.tv@gmail.com>
2023-10-18 13:52:35 +0700

    ...

---
+++ PostAndPreIncrement/Program.cs
@@ -0,0 +1,20 @@
+﻿int count = 0;
+
+Console.WriteLine("Count++ example: ");
+// Sử dụng count++ trong vòng lặp
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(count++); // In giá trị hiện tại của count, sau đó tăng count lên 1
+}
+
+Console.WriteLine("----------------------------------------");
+count = 0;
+
+Console.WriteLine("++Count example: ");
+// Sử dụng ++count trong vòng lặp
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(++count); // Tăng count lên 1, sau đó in giá trị mới của count
+}
+
+Console.ReadLine();
