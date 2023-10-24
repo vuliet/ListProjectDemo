8f6a5c6fcb2607b2df257639a775021dcb300966
ThaiVu <thai35.tv@gmail.com>
2023-10-24 16:28:46 +0700

    update

---
+++ FindMissingNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿var array = new int[] { 1, 2, 3, 4, 6, 10 };
+
+var arrayAfterSort = array.Order().ToList();
+
+var missingNumbers = new List<int>();
+
+for (var i = 0; i < arrayAfterSort.Count; i++)
+{
+    if (i == arrayAfterSort.Count - 1)
+        break;
+
+    var nextNumber = arrayAfterSort[i + 1];
+
+    var rangeBetweenTwoIndex = nextNumber - arrayAfterSort[i];
+    if (rangeBetweenTwoIndex == 1)
+        continue;
+
+    for (var j = 1; j <= rangeBetweenTwoIndex - 1; j++)
+        missingNumbers.Add(arrayAfterSort[i] + j);
+}
+
+Console.WriteLine($"Mang hien tai: {string.Join(",", array)}");
+
+if (missingNumbers.Count > 0)
+{
+    Console.Write("Missing number is: ");
+
+    foreach (var item in missingNumbers)
+        Console.Write($"{item}, ");
+
+    Console.WriteLine();
+}
+
+Console.ReadLine();
+
+
---
+++ SymmetryArray/Program.cs
@@ -0,0 +1,19 @@
+﻿var array = new int[] { 0, 1, 2, 3, 5, 5, 3, 2, 1, 0 };
+
+bool isSymmetry = true;
+
+for (int i = 0; i < array.Length / 2; i++)
+{
+    if (array[i] != array[array.Length - 1 - i])
+    {
+        Console.WriteLine("Mang khong doi xung");
+        isSymmetry = false;
+        break;
+    }
+}
+
+if (isSymmetry)
+    Console.WriteLine("Mang doi xung");
+
+Console.ReadLine();
+
