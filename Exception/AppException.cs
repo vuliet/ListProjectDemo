2e5777a63ee8df7276dc60f58d6cee5b4620952c
ThaiVu <thai35.tv@gmail.com>
2023-10-25 14:13:32 +0700

    exception demo

---
+++ Exception/AppException.cs
@@ -0,0 +1,10 @@
+﻿namespace ExceptionTest
+{
+    public class AppException : Exception
+    {
+        public AppException(string message) :
+            base(message)
+        {
+        }
+    }
+}
---
+++ Exception/Program.cs
@@ -0,0 +1,34 @@
+﻿using ExceptionTest;
+
+static bool TestException()
+{
+    throw new AppException("Exception1");
+}
+
+static bool MethodCall()
+{
+    var isException = TestException();
+
+    return isException;
+}
+
+static bool MethodCall2()
+{
+    try
+    {
+        var isException2 = MethodCall();
+
+        return isException2;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        return false;
+    }
+}
+
+var isException = MethodCall2();
+
+Console.WriteLine($"Exception:{isException}");
+
+Console.ReadLine();
