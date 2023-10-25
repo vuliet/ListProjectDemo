8f7e0410a82ff642e6d56cc763bd05e239bf3c78
ThaiVu <thai35.tv@gmail.com>
2023-10-25 11:47:48 +0700

    update

---
+++ Delegate/Program.cs
@@ -0,0 +1,20 @@
+﻿public delegate void PrintMessage(string message, string name);
+
+public class Program
+{
+    static void Main()
+    {
+        PrintMessage printHello = (message, name) => Console.WriteLine($"{message}, {name}");
+        PrintMessage printGoodbye = (message, name) => Console.WriteLine($"{message}, {name}");
+
+        GreetPerson("Hello", "John", printHello);
+        GreetPerson("Goodbye", "Alice", printGoodbye);
+
+        Console.ReadLine();
+    }
+
+    static void GreetPerson(string message, string name, PrintMessage printDelegate)
+    {
+        printDelegate(message, name);
+    }
+}
