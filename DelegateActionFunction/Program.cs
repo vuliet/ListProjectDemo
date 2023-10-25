a2959df9baedee30467cf8e2679afea5ed173338
ThaiVu <thai35.tv@gmail.com>
2023-10-25 11:42:54 +0700

    delegate

---
+++ DelegateActionFunction/Program.cs
@@ -0,0 +1,20 @@
+﻿public delegate void PrintMessage(string message);
+
+public class Program
+{
+    static void Main()
+    {
+        PrintMessage printHello = (message) => Console.WriteLine($"Hello, {message}");
+        PrintMessage printGoodbye = (message) => Console.WriteLine($"Goodbye, {message}");
+
+        GreetPerson("John", printHello);
+        GreetPerson("Alice", printGoodbye);
+
+        Console.ReadLine();
+    }
+
+    static void GreetPerson(string name, PrintMessage printDelegate)
+    {
+        printDelegate(name);
+    }
+}
