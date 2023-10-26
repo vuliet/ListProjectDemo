957690acb89a707adb461cbbf683426fdbe061f7
ThaiVu <thai35.tv@gmail.com>
2023-10-26 09:58:37 +0700

    Copy example

---
+++ CopyObject/Program.cs
@@ -0,0 +1,34 @@
+﻿using CopyObject;
+using Newtonsoft.Json;
+
+static T? DeepCopy<T>(T obj)
+{
+    if (obj is null)
+        return default;
+
+    string json = JsonConvert.SerializeObject(obj);
+    T? copy = JsonConvert.DeserializeObject<T>(json);
+
+    return copy;
+}
+
+var originObj = new AppModels()
+{
+    Id = 1,
+    Name = "Thai"
+};
+
+var newObj = DeepCopy(originObj);
+
+var listNewObj = new List<AppModels>();
+
+listNewObj.Add(originObj);
+
+listNewObj.First().Name = "Heo";
+
+if (ReferenceEquals(originObj, listNewObj.First()))
+    Console.WriteLine("obj1 và obj2 tham chieu den cung mot doi tuong.");
+else
+    Console.WriteLine("obj1 và obj2 tham chieu den doi tuong khác nhau.");
+
+Console.ReadLine();
