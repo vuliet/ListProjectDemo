3259e9744971e4c75be57c412ef14881bd9140ef
ThaiVu <thai35.tv@gmail.com>
2023-10-26 10:04:28 +0700

    update clone object

---
+++ CloneObject/Models.cs
@@ -0,0 +1,24 @@
+﻿namespace CloneObject
+{
+    public class Person : ICloneable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public object Clone()
+        {
+            return new Person(Name, Age);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Age} years old";
+        }
+    }
+}
---
+++ CloneObject/Program.cs
@@ -0,0 +1,13 @@
+﻿using CloneObject;
+
+Person person1 = new Person("John", 30);
+
+Person person2 = (Person)person1.Clone();
+
+person2.Name = "Alice";
+person2.Age = 25;
+
+Console.WriteLine("Person 1: " + person1); // John, 30 years old
+Console.WriteLine("Person 2: " + person2); // Alice, 25 years old
+
+Console.ReadLine();
