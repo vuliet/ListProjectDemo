8601808d5d20c4095339097b4d0e561d8d361d8c
ThaiVu <thai35.tv@gmail.com>
2023-11-17 11:34:47 +0700

    ...

---
+++ ConcurrentDictionary/ConcurentProtect.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Concurrent;
+
+namespace ConcurrentDictionary
+{
+    public class ConcurentProtect
+    {
+        private static readonly Dictionary<string, ConcurrentDictionary<string, long>> ProtectActionData = new();
+
+        public static bool CheckHasProtectAction(string action, string entity, int circleInSeconds = 60)
+        {
+            var now = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            lock (ProtectActionData)
+            {
+                if (ProtectActionData.TryGetValue(action, out var requests))
+                {
+                    if (requests.TryGetValue(entity, out var last))
+                    {
+                        if (last > now - TimeSpan.FromSeconds(circleInSeconds).TotalMilliseconds)
+                            return true;
+
+                        requests.TryRemove(entity, out long time);
+
+                        if (requests.IsEmpty)
+                            ProtectActionData.Remove(action);
+                    }
+                    else
+                    {
+                        requests.TryAdd(entity, now);
+                    }
+                }
+                else
+                {
+                    var newRequests = new ConcurrentDictionary<string, long>();
+                    newRequests.TryAdd(entity, now);
+                    ProtectActionData.TryAdd(action, newRequests);
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ConcurrentDictionary/Program.cs
@@ -0,0 +1,9 @@
+﻿using ConcurrentDictionary;
+
+ConcurentProtect.CheckHasProtectAction("test", "1", 5);
+ConcurentProtect.CheckHasProtectAction("test", "2", 5);
+
+Task.Delay(5000).Wait();
+
+ConcurentProtect.CheckHasProtectAction("test", "1", 5);
+ConcurentProtect.CheckHasProtectAction("test", "2", 5);
